936340da3a3f2729d933819acca65681435a4802
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-18 01:27:50 -0800

    s

--- Advanced Security/Class1.cs
+++ Advanced Security/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +14,76 @@ using Saving;
 
 namespace Advanced_Security
 {
-    public class Class1 : IOnPlayerConnectedLate, IOnPlayerDisconnected
+    public class Class1 : IOnPlayerConnectedLate, IOnAssemblyLoaded
     {
+        List<ColonyPlayerList> activeColonies;
+
+        public void OnAssemblyLoaded(string path)
+        {
+            activeColonies = new List<ColonyPlayerList>();
+        }
+
         public void OnPlayerConnectedLate(Players.Player player)
         {
-            WorldDB worldDataBase = ServerManager.SaveManager.WorldDataBase;
 
-            if (worldDataBase == null) return;
-
-
-            if (worldDataBase.TryGetWorldKeyValue(player.ID.SteamID.ToString(), out JToken jDifficulty) && jDifficulty != null)
+            for (int i = 0; i < player.ColonyGroups.Count; i++)
             {
-                string[] playerColonyDiffiuclties = JsonConvert.DeserializeObject<string[]>(jDifficulty.ToString());
+                //string colonyID = colonyPlayerLists.Where(colony => colony.colonyID == player.ColonyGroups[i].ColonyGroupID.ToString());
+                //int index = colonyPlayerLists.IndexOf(new ColonyPlayerList(player.ColonyGroups[i].ColonyGroupID.ToString()));
 
-                for (int i = 0; i < playerColonyDiffiuclties.Length; i++)
+                bool foundActiveColony = false;
+                for (int i2 = 0; i2 < activeColonies.Count; i2++)
                 {
-                    player.ColonyGroups[i].DifficultySetting.Key = playerColonyDiffiuclties[i];
-                    Log.Write("Player joining, setting the difficulty of colony '" + player.ColonyGroups[i].Name + "' (Owned by: " + player.Name + ") to index " + playerColonyDiffiuclties[i]);
-                    //Log.Write("Player: '" + player.Name + "' has joined, setting difficulty for their colony (" + player.ColonyGroups[i].Name + ") to " + playerColonyDiffiuclties[i]);
+                    if (player.ColonyGroups[i].ColonyGroupID.ToString() == activeColonies[i].colonyID)
+                    {
+                        activeColonies[i].onlinePlayers.Add(player.ID.SteamID.ToString());
+                        foundActiveColony = true;
+                        Log.Write("Adding player to existing colony list");
+                        break;
+                    }
+                }
+
+                if (!foundActiveColony)
+                {
+                    Log.Write("Creating new entry");
+                    activeColonies.Add(new ColonyPlayerList(player.ColonyGroups[i].ColonyGroupID.ToString(), player.ID.SteamID.ToString()));
                 }
             }
+            //    Log.Write(player.ColonyGroups[i].ColonyGroupID + " erhgughuegr");
+            //    if (index != -1)
+            //    {
+            //        // Add player to existing colony list
+            //        Log.Write("Adding player to existing colony list");
+            //        colonyPlayerLists[index].onlinePlayers.Add(player.ID.SteamID.ToString());
+            //    }
+            //    else
+            //    {
+            //        // Create new colony list for players colony
+            //        Log.Write("Creating new entry");
+            //        colonyPlayerLists.Add(new ColonyPlayerList(player.ColonyGroups[i].ColonyGroupID.ToString(), player.ID.SteamID.ToString()));
+
+            //        //colonyPlayerLists[colonyPlayerLists.Count - 1].onlinePlayers.Add(player.ID.SteamID.ToString());
+
+            //    }
+            //}
+
+
+            //WorldDB worldDataBase = ServerManager.SaveManager.WorldDataBase;
+
+            //if (worldDataBase == null) return;
+
+
+            //if (worldDataBase.TryGetWorldKeyValue(player.ID.SteamID.ToString(), out JToken jDifficulty) && jDifficulty != null)
+            //{
+            //    string[] playerColonyDiffiuclties = JsonConvert.DeserializeObject<string[]>(jDifficulty.ToString());
+
+            //    for (int i = 0; i < playerColonyDiffiuclties.Length; i++)
+            //    {
+            //        player.ColonyGroups[i].DifficultySetting.Key = playerColonyDiffiuclties[i];
+            //        Log.Write("Player joining, setting the difficulty of colony '" + player.ColonyGroups[i].Name + "' (Owned by: " + player.Name + ") to index " + playerColonyDiffiuclties[i]);
+            //        //Log.Write("Player: '" + player.Name + "' has joined, setting difficulty for their colony (" + player.ColonyGroups[i].Name + ") to " + playerColonyDiffiuclties[i]);
+            //    }
+            //}
         }
 
         public void OnPlayerDisconnected(Players.Player player)
---
+++ Advanced Security/ColonyPlayerList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Security
+{
+    public class ColonyPlayerList
+    {
+        public string colonyID;
+
+        public List<string> onlinePlayers = new List<string>();
+
+        public ColonyPlayerList(string _colonyID, string playerID = "")
+        {
+            colonyID = _colonyID;
+
+            if(playerID != "")
+                onlinePlayers.Add(playerID);
+        }
+    }
+}
